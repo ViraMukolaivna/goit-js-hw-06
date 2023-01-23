7229612b063644bf64330919b4dccc965d2d5d4c
ViraMukolaivna <virginia16.66.66@gmail.com>
2023-01-23 19:39:51 +0200

    have made the 6 task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,14 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент < li >.
+//   Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі < li > за одну операцію 
+// у список ul#ingredients.
+
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +17,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElem = document.getElementById("ingredients");
+for (const ingredient of ingredients) {
+  const liElem = document.createElement("li");
+  liElem.classList.add("item");
+  liElem.textContent = ingredient;
+  ulElem.appendChild(liElem);
+}
